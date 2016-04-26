91a37485e57b01930d306ebf495b909ecd7ffab6
KweonDH <mylove5249@nate.com>
2016-04-26 14:43:43 +0900

    Update javalang.java

--- src/javalang/javalang.java
+++ src/javalang/javalang.java
@@ -16,6 +16,73 @@ public class javalang {
         // TODO: cal의 내용을 Korean이 true/false에 따라 아래와 같이 출력하기.
         // true인 경우: 2016년 4월 26일 화요일 14시 12분
         // false인 경우: 26-4-2016 Tue. 14:12
+        Calendar now = Calendar.getInstance();
+        int year = now.get(Calendar.YEAR);
+        int month = now.get(Calendar.MONTH)+1;
+        int day = now.get(Calendar.DAY_OF_MONTH);
+        int hour = now.get(Calendar.HOUR_OF_DAY);
+        int min = now.get(Calendar.MINUTE);
+        int week = now.get(Calendar.DAY_OF_WEEK);
+        if(Korean){
+            StringBuffer KrDay = new StringBuffer(year + "년 " + month + "월 " + day + "일 ");
+            switch(week) {
+                case Calendar.SUNDAY:
+                    KrDay.append("일요일");
+                    break;
+                case Calendar.MONDAY:
+                    KrDay.append("월요일");
+                    break;
+                case Calendar.TUESDAY:
+                    KrDay.append("화요일");
+                    break;
+                case Calendar.WEDNESDAY:
+                    KrDay.append("수요일");
+                    break;
+                case Calendar.THURSDAY:
+                    KrDay.append("목요일");
+                    break;
+                case Calendar.FRIDAY:
+                    KrDay.append("금요일");
+                    break;
+                case Calendar.SATURDAY:
+                    KrDay.append("토요일");
+                    break;
+            }
+            KrDay.append(" " + hour + "시 " + min + "분");
+            System.out.println(KrDay);
+        }
+        else {
+            StringBuffer EnDay = new StringBuffer(day + "-" + month + "-" + year + " ");
+            switch(week) {
+                case Calendar.SUNDAY:
+                    EnDay.append("Sun.");
+                    break;
+                case Calendar.MONDAY:
+                    EnDay.append("Mon.");
+                    break;
+                case Calendar.TUESDAY:
+                    EnDay.append("Tue.");
+                    break;
+                case Calendar.WEDNESDAY:
+                    EnDay.append("Wed.");
+                    break;
+                case Calendar.THURSDAY:
+                    EnDay.append("Thu.");
+                    break;
+                case Calendar.FRIDAY:
+                    EnDay.append("Fri.");
+                    break;
+                case Calendar.SATURDAY:
+                    EnDay.append("Sat.");
+                    break;
+            }
+            EnDay.append(" " + hour + ":" + min);
+            System.out.println(EnDay);
+        }
+    }
+
+    public String toString(){
+        return "HelloJava";
     }
 
     public static void main(String[] args) {
@@ -24,13 +91,15 @@ public class javalang {
         float sum = 0;
         for (int i = 0; i < tokens.length; i++) {
             System.out.println(tokens[i].trim());
+            sum+= Float.parseFloat(tokens[i].trim());
         }
         // TODO: input 문자열의 숫자를 모두 합하여 sum에 넣기.
         System.out.println("sum:" + sum);
 
         // TODO: input2 문자열에서 .을 !로 바꾸기
         // Hint: String.replace()
-        String new_input2 = input2; // 이 부분을 고칠 것.
+        String new_input2 = input2;
+        new_input2 = input2.replace(".", "!");// 이 부분을 고칠 것.
         System.out.println(new_input2);
 
         // TODO: 아래 문장 수행결과로 HelloJava 가 출력되도록 class javalng에 메소드를 추가하기.
